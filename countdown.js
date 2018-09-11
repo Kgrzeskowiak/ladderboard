844629391854605fc49afdb5cf528a12a5258fa1
Kgrzeskowiak <Kgrzeskowiak@users.noreply.github.com>
2018-09-11 23:04:16 +0200

    timer

--- countdown.js
+++ countdown.js
@@ -1,23 +1,30 @@
 class Countdown{
     constructor()
     {
-
     }
-initializeClock(gameDuration)
+initializeClock(gameDuration, action)
 {
     var clock = document.getElementById("clockdiv")
     var minutes = clock.querySelector(".minutes");
-    var seconds = clock.querySelector(".seconds");
+    var seconds = clock.querySelector(".seconds"); 
     var startCounting = setInterval(counting, 1000);
     gameDuration = Math.floor(gameDuration * 60);
-function counting(){
-    gameDuration = gameDuration -1;
-    minutes.innerHTML = Math.floor(gameDuration / 60);
-    seconds.innerHTML = Math.floor(gameDuration % 60);
-    if (gameDuration <= 0)
-    {
-    clearInterval(startCounting);
-    }
+        function counting()
+        {
+        gameDuration = gameDuration -1;
+        minutes.innerHTML = Math.floor(gameDuration / 60);
+        seconds.innerHTML = Math.floor(gameDuration % 60);
+            if (gameDuration <= 0)
+            {
+            clearInterval(startCounting);
+            }
+            if (action == "stop")
+            {
+            clearInterval(startCounting -1);
+            minutes.innerHTML = 0;
+            seconds.innerHTML = 0;
+            }
+        }
+} 
 }
-}
-}
+ 
--- startPanel.js
+++ startPanel.js
@@ -11,17 +11,23 @@ class StartPanel extends Panel{
       var templateClone = document.importNode(template.content, true);
       var configurationButton = templateClone.querySelector("[data-name='ConfigurationPanelButton']");
       var gameHandler = templateClone.querySelector("[data-name='GameHandler']");
-      var startGameButton = gameHandler.querySelector("button");
+      var startGameButton = gameHandler.querySelector("[data-name='StartGame']");
+      var stopGameButton = gameHandler.querySelector("[data-name='StopGame']")
       root.appendChild(templateClone);
       var clock = new Countdown()
       configurationButton.addEventListener("click", event =>
     {
-        this.app.sendAction("ConfigurationPanelRequested");   
+        this.app.sendAction("ConfigurationPanelRequested");
+        clock = null;   
     })
     startGameButton.addEventListener("click", event =>
     {
         var gameDuration = gameHandler.querySelector("input").value;
-        clock.initializeClock(gameDuration);
+        clock.initializeClock(gameDuration, "start");
+    })
+    stopGameButton.addEventListener("click", event =>
+    {
+        clock.initializeClock(0,"stop");
     })
       
     }
